4a46f4816942731e9bd12efb2ca4e107828f6e51
Dmitriy Mamykin <dmitriymamykin@Dmitriys-MacBook-Air.local>
2024-02-13 04:52:40 -0800

    interfaces done

--- golang/cmd/main.go
+++ golang/cmd/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	. "golang/pkg/chapters/chapter7"
+)
+
 func main() {
+	filename := "file.xml"
+
+	root, err := ConstructTree("file.xml")
+	if err != nil {
+		fmt.Printf("constructing tree from file: %s; %s", filename, err)
+	}
+
+	PrintTree(root)
 }
